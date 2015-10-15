a31f2e459afc32fcac4b005dc00c59ac5eeddb37
Sammy Patenotte <sammy.patenotte@yahoo.com>
2015-10-14 18:30:25 -0700

    Work in progress indexedDB

--- dom/privacymonitor/PrivacyMonitor.js
+++ dom/privacymonitor/PrivacyMonitor.js
@@ -5,7 +5,7 @@ const Cc = Components.classes;
 
 Cu.import("resource://gre/modules/XPCOMUtils.jsm");
 
-function debug2(str) {
+function debug(str) {
   dump("-*- Permission WebIDL: " + str + "\n");
 }
 
@@ -14,35 +14,37 @@ function PrivacyMonitor() {
 }
 
 function writeToIndexedDB(name, permission, date) {
-  debug2("Entered indexedDB");
+  debug("Entered indexedDB");
   let request = indexedDB.open("permissionLog", 1);
   var db;
-  
+
   request.onerror = function(event) {
-    debug2("error opening DB ");
+    debug("error opening DB ");
   };
- 
+
   request.onsuccess = function(event) {
     db = event.target.result;
-    debug2(typeof(db));
-    debug2("success opening DB");
+    debug("success opening DB");
     var write = db.transaction(["entries"], "readwrite")
               .objectStore("entries")
               .add({ name: name, permission: permission, date: date});
 
     write.onerror = function(event) {
-      debug2("error writing to DB");
+      debug("error writing to DB");
     };
- 
+
     write.onsuccess = function(event) {
-      debug2("success writing to DB");
+      debug("success writing to DB");
     };
+
+    db.close();
   };
 
   request.onupgradeneeded = function(event) {
     db = event.target.result;
     var objectStore = db.createObjectStore("entries", { autoIncrement : true });
     objectStore.createIndex("name", "name", { unique: false });
+    objectStore.createIndex("permission", "permission", { unique: false });
     objectStore.createIndex("date", "date", { unique: false });
   }
 }
@@ -57,30 +59,55 @@ PrivacyMonitor.prototype = {
                                     contractID: "@mozilla.org/privacy-monitor;1",
                                     interfaces: [Ci.PrivacyMonitor],
                                     flags: Ci.nsIClassInfo.DOM_OBJECT}),
-  
+
   logPermissionRequest: function logPermissionRequest(appID, typesInfo) {
-    debug2("Entered WebIDL");
+    // Getting app info
     let app = Cc["@mozilla.org/AppsService;1"].getService(Ci.nsIAppsService).getAppByLocalId(appID);
 
     for (let i in typesInfo) {
-      debug2("Prompting for: " + typesInfo[i].permission);
+      // Logging to indexedDB for any type of permission asked
       writeToIndexedDB(app.name, typesInfo[i].permission, new Date().toISOString());
     }
-    debug2("From app: " + JSON.stringify(app.name));
-    debug2("At: " + new Date().toISOString());
+    debug("From app: " + JSON.stringify(app.name));
+    debug("At: " + new Date().toISOString());
   },
 
   readIndexedDB: function readIndexedDB() {
-    let request = indexedDB.open("permissionLog", 1);
+    debug("Running readIndexedDB");
+    let request = indexedDB.open("permissionLog");
+    debug("Trying to open DB");
+
+    request.onblocked = function(event) {
+      debug("opening blocked");
+      return null;
+    };
+
+    request.onabort = function(event) {
+      debug("opening aborted");
+      return null;
+    };
 
     request.onerror = function(event) {
-      debug2("error opening DB ");
+      debug("error opening DB");
+      return null;
     };
 
     request.onsuccess = function(event) {
-      db = event.target.result;
-      return db;
+      debug("Returning DB object");
+      // return db object
+      return event.target.result;
     };
+
+    request.onupgradeneeded = function(event) {
+      debug("Upgrading or creating DB");
+      let db = event.target.result;
+      let objectStore = db.createObjectStore("entries", { autoIncrement : true });
+      objectStore.createIndex("name", "name", { unique: false });
+      objectStore.createIndex("permission", "permission", { unique: false });
+      objectStore.createIndex("date", "date", { unique: false });
+    }
+
+    debug("Shouldn't get here");
   }
 };
 
